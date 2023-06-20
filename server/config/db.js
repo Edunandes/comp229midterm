cb3f0bc520c5fd97c832ffefe401b497c778a2af
Eduardo Fernandes <95150616+Edunandes@users.noreply.github.com>
2023-06-19 20:26:59 -0600

    Initial Commit

---
+++ server/config/db.js
@@ -0,0 +1,18 @@
+
+
+    // Author: Eduardo Fernandes
+    // ID : 301290717 
+    // June 20, 2023
+    // Centennial College - Mid term Summer 20023 
+
+
+
+
+
+
+
+
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://fernandeseduard:I1ptwVDKn8ojZFtB@books.8lpqvyc.mongodb.net/books?retryWrites=true&w=majority"
+};
